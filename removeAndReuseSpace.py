6a659f225edcce211026b805faeccaa128f2d570
PhilZeraNTC <pedro.henrique.vogt@gmail.com>
2025-05-26 02:14:24 -0300

    working on

--- removeAndReuseSpace.py
+++ removeAndReuseSpace.py
@@ -1,21 +1,34 @@
+import sys
+
 def removeRegistro(arquivo, chave):
+    with open(arquivo, 'r', encoding='utf-8') as f:
+        animes = f.readlines()
+
     for i in range(len(animes)):
         if i == chave:
-            novaLinha = '*|' + arquivo[i][2:]
-            arquivo[i] = novaLinha
-    return arquivo
-            
-    
-if __name__ == '__main__':
-    with open('outputAnimes.txt', 'r', encoding='utf-8') as f:
-        animes = f.readlines()
-        
-    animes = removeRegistro(animes, 5)
-    
+            novaLinha = '*|' + animes[i][2:]
+            animes[i] = novaLinha
+
     with open('outputRemovido.txt', 'w', encoding='utf-8') as f:
         for registro in animes:
-            print(registro)
-            f.write(str(registro))
+            f.write(registro)
     
+            
+def compactacaoDados(entrada, saida):
+    indicesVazios = []
+    with open(entrada, 'r') as base:
+        with open(saida, 'w') as saida:
+            for i,b in enumerate(base):
+                if '*|' not in b:
+                    dadoCopido = b
+                    saida.write(dadoCopido)
+                else:
+                    indicesVazios.append(i)
+    
+
+if __name__ == '__main__':
+    removeRegistro('outputAnimes.txt', 8)
+
+    compactacaoDados('outputRemovido.txt', 'outputCompactado.txt')
     
         
